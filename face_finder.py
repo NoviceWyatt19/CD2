b911379407541371635e0b7f5dff9708d79847f9
NoviceWyatt19 <aseltula596@gmail.com>
2024-10-03 14:57:35 +0900

    Oct 3rd: Minor Update - Organize a structure

--- face_finder.py
+++ face_finder.py
@@ -14,19 +14,20 @@ for person in result:
                   (bounding_box[0]+bounding_box[2], bounding_box[1] + bounding_box[3]),
                   (255, 0, 0), 2)
 
-output_path = '/Users/wyatt/Desktop/CD2_project/detected_faces.jpg'
-print("Checking if file exists:", output_path)
-
-if os.path.exists(output_path):
-    print("exist same name, delete existing file")
-    os.remove(output_path)  # 파일이 존재하면 삭제
-else:
-    print("File does not exist.")
-
-cv2.imwrite(output_path, img)
-
 if detected:
     print("Success detecting!")
+    # outpath를 while 또는 for문을 통해 cam_frame에 저장된 사진을 모두 돌게 만들면 됨
+    # 이후 실시간 캠을 인식하도록 해서 눈감기 탐지 코드에도 활용할 계획
+    output_path = '/Users/wyatt/Desktop/CD2_project/detected_faces.jpg'
+    print("Checking if file exists:", output_path)
+
+    if os.path.exists(output_path):
+        print("exist same name, delete existing file")
+        os.remove(output_path)  # 파일이 존재하면 삭제
+    else:
+        print("File does not exist.")
+
+    cv2.imwrite(output_path, img)
 else:
     print("Fail")
 
